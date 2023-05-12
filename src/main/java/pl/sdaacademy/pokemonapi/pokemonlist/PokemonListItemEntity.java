4845ac64d8be226494732759326e805f58211d52
brwngda <grz.barwinski@gmail.com>
2023-05-12 20:15:49 +0200

    feature(pa): parse pokemon id from details url

--- src/main/java/pl/sdaacademy/pokemonapi/pokemonlist/PokemonListItemEntity.java
+++ src/main/java/pl/sdaacademy/pokemonapi/pokemonlist/PokemonListItemEntity.java
@@ -1,8 +1,7 @@
 package pl.sdaacademy.pokemonapi.pokemonlist;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,13 +15,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PokemonListItemEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
-    private String pokemonDetailsUrl;
-
-    public PokemonListItemEntity(String name, String url) {
-
-    }
 }
